b5436f8dc42c8ad6ecd135ad5214ffc55d0a943e
Colin Warner <colin.x.warner@gmail.com>
2021-02-27 07:36:25 -0600

    Adding initial code to html and js

--- js/movie-app.js
+++ js/movie-app.js
@@ -1,13 +1,19 @@
 "use strict";
 
+$(document).ready(function() {
+    const API = "https://coconut-denim-leather.glitch.me/movies"
+    let title = $("#title");
+    let popcorns = $("#popcorns");
+})
 
+//TEST IN CONSOLE
     const getMovies = fetch("https://coconut-denim-leather.glitch.me/movies")
         .then(response => console.log(response.json()));
 
     //Print JSON data to header
-    $("#button").click(function (getMovies) {
-        $(this).text(getMovies).val()
-    });
+    // $("#button").click(function (getMovies) {
+    //     $(this).text(getMovies).val()
+    // });
 {
     //Create - allow the user to add movie title and their rating
     //$("#user-movie-button").click
@@ -57,3 +63,35 @@ That is, if we want to delete or modify an existing movie, we can specify what m
 When a new movie is created (i.e. when you send a POST request to /movies with a title and a rating), the server will respond with the movie object that was created,
 including a generated id. Take a look at the other branches in this repository, as they have configuration/setup for common scenarios,
 such as including bootstrap in your application.*/
+
+/*
+Casey Edwards  12:21
+@channel Helpful structural concepts here:
+To create this application, you need to
+-Not worry about the visual aspect until your code itself is fleshed out!
+-Create the CRUD functions (and make sure each thing you type works before adding another piece)
+(console.log)
+-Call the CRUD functions
+(console.log)
+-Use some jQuery/DOM to add and remove things from the screen
+(console.log)
+-Create a form to allow the user to search!
+(console.log)
+-Use some jQuery/DOM to allow the user to search
+(console.log)
+-STYLE IT last
+(console.log)
+And whenever something goes wrong (it will)
+(console.log)
+(make sure you're using the methods like fetch, etc as is shown in documentation)
+(console.log)
+If THAT doesn't work, try this:
+ -Isolate what exactly isn't working (the specific function)
+ -Find the point in that code where things do work
+ -SLOWLY, CAREFULLY add code back in, console logging with each small step until things break
+ -When you find that breaking point, and each single piece before it worked -> that's probably where the issue is <-
+ -Ask yourself if you called the method appropriately. Passed in the correct data type, returned the correct data type, called it in the correct place, etc.
+ -If not, check the documentation, curriculum, and lecture notes. Chances are, you may have missed some smol detail!
+I'm a firm believer that complicated things can be accomplished if they are broken into easy tasks and we take a methodical approach to making sure each tiny piece works before moving on!
+SAVE THIS. PUT IT SOMEWHERE AND CONSTANTLY LOOK BACK AT IT WHEN YOU GET STUCK
+ */
